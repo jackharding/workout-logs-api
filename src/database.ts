717306a482c2efa23aeb5fae5b595f4fe258a779
Jack Harding <jackk.harding.93@gmail.com>
2024-03-23 20:06:05 +0000

    adds kysely

--- src/database.ts
+++ src/database.ts
@@ -1,27 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO: DB connection fails when I move this to index.ts
 import 'dotenv/config';
+import { Kysely, PostgresDialect } from 'kysely';
+import { DB } from 'kysely-codegen';
 import pg, { QueryResult } from 'pg';
 
-const pool = new pg.Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+const dialect = new PostgresDialect({
+  pool: new pg.Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  }),
 });
 
-export const query = async (
-  queryText: string,
-  values?: any[]
-): Promise<QueryResult> => {
-  const start = Date.now();
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
+// to communicate with your database.
+export const db = new Kysely<DB>({
+  dialect,
+});
 
-  const res = await pool.query(queryText, values);
-  const duration = Date.now() - start;
+// export const query = async (
+//   queryText: string,
+//   values?: any[]
+// ): Promise<QueryResult> => {
+//   const start = Date.now();
 
-  console.log('executed query', { queryText, duration, rows: res.rowCount });
-  return res;
-};
+//   const res = await pool.query(queryText, values);
+//   const duration = Date.now() - start;
+
+//   console.log('executed query', { queryText, duration, rows: res.rowCount });
+//   return res;
+// };
 
 // export const connectToDatabase = async () => {
 //   try {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { Exercise, Log } from './types.js';
-import * as db from './database.js';
+import { db } from './database.js';
 import { buildLogExercisesQuery } from './utils/queryBuilders.js';
 // import { client } from './database';
 
@@ -28,18 +28,21 @@ const exercises: Array<Exercise> = [
 ];
 
 app.get('/exercise-categories', async (req, res) => {
-  const results = await db.query('SELECT * from exercise_category');
-  res.json(results.rows);
+  const results = await db
+    .selectFrom('exercise_category')
+    .selectAll()
+    .execute();
+  res.json(results);
 });
 
 app.get('/exercises', async (req, res) => {
-  const results = await db.query('SELECT * from exercise');
-  res.json(results.rows);
+  const results = await db.selectFrom('exercise').selectAll().execute();
+  res.json(results);
 });
 
 app.get('/logs', async (req, res) => {
-  const results = await db.query('SELECT * from log');
-  res.json(results.rows);
+  const results = await db.selectFrom('log').selectAll().execute();
+  res.json(results);
 });
 
 /**
@@ -51,41 +54,40 @@ app.get('/logs', async (req, res) => {
 app.post('/logs', async (req, res) => {
   const { name, dateStart, dateEnd, notes, exercises } = req.body as Log;
 
-  const newLogAlias = 'new_log';
+  const { logId } = await db
+    .insertInto('log')
+    .values({
+      name,
+      notes,
+      date_start: dateStart,
+      date_end: dateEnd,
+    })
+    .returning('id as logId')
+    .executeTakeFirst();
 
-  const query = `
-  WITH ${newLogAlias} AS (
-    INSERT INTO log(name, date_start, date_end, notes) VALUES($1, $2, $3, $4) RETURNING id
-  )
-  ${buildLogExercisesQuery(newLogAlias, exercises)};
-`;
+  // TODO: How do I do this using with???
+  await db
+    .insertInto('log_exercise')
+    .values(
+      exercises.map((exercise) => ({
+        log_id: logId,
+        exercise_id: exercise.exerciseId,
+        notes: exercise.notes,
+      }))
+    )
+    .execute();
 
-  console.log(query);
-
-  const results = await db.query(query, [name, dateStart, dateEnd, notes]);
-
-  // const logId = results.rows[0].id;
-
-  // const logExercisesQuery = buildLogExercisesQuery(logId, exercises);
-
-  // // TODO: most definitely not the way to do this
-  // const resultss = await db.query(logExercisesQuery, []);
-
-  res.json(results.rows);
+  res.json();
 });
 
 app.post('/exercises', async (req, res) => {
-  const { name, categoryId, bodyPartId, icon, notes, video } =
-    req.body as Exercise;
+  // const { name, categoryId, bodyPartId, icon, notes, video } =
+  //   req.body as Exercise;
 
-  try {
-    await db.query(
-      'INSERT INTO exercise(name, body_part_id, category_id, icon, notes, video) VALUES($1, $2, $3, $4, $5, $6)',
-      [name, bodyPartId, categoryId, icon, notes, video]
-    );
-  } catch (err) {
-    console.log(err);
-  }
+  db.insertInto('exercise')
+    .values(req.body)
+    .returningAll()
+    .executeTakeFirstOrThrow();
 
   res.json(null);
 });
